22edc9efdea61cefe529709f5a6a05b3be4184a3
datt16 <osatom.4639@gmail.com>
2021-11-22 00:06:52 +0900

    :+1: station15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,19 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = await test(userList)
   return await result
 }
 
-function test() {
-  return
+function test(data) {
+  const res = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      data.map(
+        (obj) => (obj.full_name = `${obj.family_name} ${obj.first_name}`)
+        );
+      resolve(data)
+    }, 3000)
+  })
+  return res
 }
 
